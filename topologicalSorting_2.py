a9053512fb885715ec853897201eff043a81b24f
janghana <han001112@icloud.com>
2022-12-11 23:43:58 +0900

    topologicalSorting_2.py

---
+++ topologicalSorting_2.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+height_students = [[] for _ in range(N + 1)]
+in_degree = [0] * N
+
+for _ in range(M):
+    A, B = map(int, input().rstrip().split())
+    height_students[A].append(B)
+    in_degree[B] += 1
+
+def topologicalSorting(height_students):
+    data_queue = deque()
+    result = []
+
+    for i in range(0, N):
+        if in_degree[i] == 0:
+            data_queue.append(i)
+
+    while data_queue:
+        current_val = data_queue.popleft()
+        result.append(current_val)
+
+        for i in height_students[current_val]:
+            in_degree[i] -= 1
+            if in_degree[i] == 0:
+                data_queue.append(i)
+    for i in result:
+        print(i, end=" ")
+
+
+topologicalSorting(height_students)
