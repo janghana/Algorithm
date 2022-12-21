d46b4670e4c4b7121daab1b92a5b20f8b51abb36
janghana <han001112@icloud.com>
2022-12-21 21:44:52 +0900

    upload function_2.py

--- Python/function/function_2.py
+++ Python/function/function_2.py
@@ -1,3 +1,13 @@
-def solve(a):
-    ans = 0
-    return sum(a)
+result_li = [i for i in range(1,10000)]
+tmp_li = []
+for i in range(1,10000):
+    tmp_val = 0
+    for j in str(i):
+        tmp_val += int(j)
+    tmp_val += i
+    if tmp_val < 10000:
+        tmp_li.append(tmp_val)
+for i in list(set(tmp_li)):
+    result_li.remove(i)
+for i in result_li:
+    print(i)
