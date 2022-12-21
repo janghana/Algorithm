5e4211744d44e7761e82e45b37122fb4aeb0dca3
janghana <han001112@icloud.com>
2022-12-21 16:09:11 +0900

    upload one-dimensional_array_3.py

--- Python/one-dimensional_array/one-dimensional_array_3.py
+++ Python/one-dimensional_array/one-dimensional_array_3.py
@@ -1,11 +1,14 @@
-input()
-num_list = list(map(int, input().split()))
-min_ = 10000000
-max_ = -10000000
-for i in num_list:
-    if min_ > i:
-        min_ = i
-    if max_ < i:
-        max_ = i
-
-print(min_, max_)
+count = 0
+max_ = -100000000
+cnt = 0
+while True:
+    try:
+        count+=1
+        N = int(input())
+        if N > max_:
+            max_ = N
+            cnt = count
+    except:
+        break
+print(max_)
+print(cnt)
