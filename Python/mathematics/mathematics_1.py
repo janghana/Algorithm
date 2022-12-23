ff6a7bc6eb804cdf9732756171dea14f20e2b32b
janghana <han001112@icloud.com>
2022-12-23 16:10:43 +0900

    upload mathematics_1.py

--- Python/mathematics/mathematics_1.py
+++ Python/mathematics/mathematics_1.py
@@ -1,1 +1,15 @@
-print(ord(input()))
+default_cost, factory_cost, product_cost = map(int, input().split())
+
+cnt = 0
+if factory_cost >= product_cost:
+    print(-1)
+else:
+    while True:
+        cnt += 10000
+        if default_cost + factory_cost * cnt < product_cost * cnt:
+            break
+    while True:
+        cnt -= 1
+        if default_cost + factory_cost * cnt >= product_cost * cnt:
+            break
+    print(cnt+1)
