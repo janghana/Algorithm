5022b6b74ae91ffd27de2fac8f8bb76b573dcb81
janghana <han001112@icloud.com>
2022-12-26 22:01:02 +0900

    upload tow-dimensional_array_1.py

--- Python/two-dimensional_array/two-dimensional_array_1.py
+++ Python/two-dimensional_array/two-dimensional_array_1.py
@@ -1,12 +1,10 @@
 N, M = map(int, input().split())
-
 matrix_A = [[] for _ in range(N)]
 matrix_B = [[] for _ in range(N)]
 for i in range(N):
     A_list = list(map(int ,input().split()))
     for j in range(M):
         matrix_A[i] = A_list
-
 for i in range(N):
     B_list = list(map(int ,input().split()))
     for j in range(M):
