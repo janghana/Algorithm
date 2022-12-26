463d03c446e4746c8786589137d32a1f0c17a8ea
janghana <han001112@icloud.com>
2022-12-26 20:40:54 +0900

    mathematics2_special.py

---
+++ Python/mathematics2/mathematics2_special.py
@@ -0,0 +1,14 @@
+from sys import stdin
+arr = [True] * 1000001
+for i in range(2, 1001):
+    if arr[i]:
+        for j in range(i + i,1000001, i):
+            arr[j] = False
+while True:
+    A = int(stdin.readline())
+    if A == 0:
+        break
+    for i in range(3,len(arr)):
+        if arr[i] and arr[A-i]:
+            print(A, "=", i, "+", A - i)
+            break
