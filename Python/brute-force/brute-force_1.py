ebf7bc7aa3b915a34985957c835317384bbe510d
janghana <han001112@icloud.com>
2022-12-28 18:12:19 +0900

    brute-force_1.py

--- Python/brute-force/brute-force_1.py
+++ Python/brute-force/brute-force_1.py
@@ -1,9 +1,20 @@
-N = int(input())
+from itertools import combinations
 
-def factorial(N):
-    if N == 0:
-        return 1
-    if N == 1:
-        return 1
-    return N * factorial(N-1)
-print(factorial(N))
+N, M = map(int, input().split())
+blackjak_list = list(map(int, input().split()))
+sum_list = []
+check_equal = False
+result = M
+for i in list(combinations(blackjak_list, 3)):
+    tmp_val = sum(list(i))
+    if tmp_val == M:
+        check_equal = True
+        break
+    elif tmp_val < M:
+        sum_list.append(M - tmp_val)
+if check_equal:
+    print(result)
+else:
+    sum_list.sort()
+    result -= sum_list[0]
+    print(result)
