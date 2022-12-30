740b22bbe118248aee2e3089914e8b7c8a522716
janghana <han001112@icloud.com>
2022-12-30 14:12:13 +0900

    upload set_map_1.py

--- Python/set_map/set_map_1.py
+++ Python/set_map/set_map_1.py
@@ -1,20 +1,11 @@
-from itertools import combinations
-
-N, M = map(int, input().split())
-blackjak_list = list(map(int, input().split()))
-sum_list = []
-check_equal = False
-result = M
-for i in list(combinations(blackjak_list, 3)):
-    tmp_val = sum(list(i))
-    if tmp_val == M:
-        check_equal = True
-        break
-    elif tmp_val < M:
-        sum_list.append(M - tmp_val)
-if check_equal:
-    print(result)
-else:
-    sum_list.sort()
-    result -= sum_list[0]
-    print(result)
+N = int(input())
+N_list = set(map(int,input().split()))
+M = int(input())
+M_list = list(map(int,input().split()))
+result = []
+for i in M_list:
+    if i not in N_list:
+        result.append(0)
+    else:
+        result.append(1)
+print(*result)
