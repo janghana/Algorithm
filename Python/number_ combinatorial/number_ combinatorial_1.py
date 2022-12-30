e7b30927d0f8d5560f5a83223e4433c88baae446
janghana <han001112@icloud.com>
2022-12-31 01:28:40 +0900

    upload number_combinatorial.py

--- Python/number_ combinatorial/number_ combinatorial_1.py
+++ Python/number_ combinatorial/number_ combinatorial_1.py
@@ -1,10 +1,16 @@
-x, y, w, h = map(int, input().split())
-min_val_list = []
-min_val_list.append(x)
-min_val_list.append(y)
-min_val_list.append(abs(x-w))
-min_val_list.append(abs(y-h))
-min_val_list.append(((x)**2+(y)**2)**0.5)
-min_val_list.append(((abs(w-x))**2+(abs(h-y))**2)**0.5)
-
-print(min(min_val_list))
+while True:
+    a, b = map(int ,input().split())
+    TF_factor = True
+    TF_multiple = True
+    if a == 0 and b == 0:
+        break
+    if b % a != 0 or a >=  b:
+        TF_factor = False
+    if a % b != 0 or b >= a or a // b == 0:
+        TF_multiple = False
+    if TF_factor and not TF_multiple:
+        print("factor")
+    elif not TF_factor and TF_multiple:
+        print("multiple")
+    elif not TF_factor and not TF_multiple:
+        print("neither")
