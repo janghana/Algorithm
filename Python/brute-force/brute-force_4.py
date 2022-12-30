aae76f92327dff3a83216c14786546a87232fb76
janghana <han001112@icloud.com>
2022-12-30 13:54:33 +0900

    upload brute-force_4.py

--- Python/brute-force/brute-force_4.py
+++ Python/brute-force/brute-force_4.py
@@ -1,9 +1,25 @@
-M, N = map(int, input().split())
+N ,M = map(int, input().split())
 black_and_white = []
 for i in range(N):
     black_and_white.append(input())
 
-for i in black_and_white:
-    for j in i:
-
-
+result = []
+for x in range(N - 7):
+    for y in range(M - 7):
+        start_with_B = 0
+        start_with_W = 0
+        for i in range(8):
+            for j in range(8):
+                if (x+i + y+j) % 2 == 0:
+                    if black_and_white[x+i][y+j] != "B":
+                        start_with_W += 1
+                    if black_and_white[x+i][y+j] != "W":
+                        start_with_B += 1
+                else:
+                    if black_and_white[x+i][y+j] != "W":
+                        start_with_W += 1
+                    if black_and_white[x+i][y+j] != "B":
+                        start_with_B += 1
+        result.append(start_with_W)
+        result.append(start_with_B)
+print(min(result))
