1bbd65b51167dbf0f7451c8a992eaf8d19abdd3e
janghana <han001112@icloud.com>
2023-01-01 15:26:15 +0900

    upload number_combinatorial_3.py

---
+++ Python/number_ combinatorial/number_ combinatorial_3.py
@@ -0,0 +1,16 @@
+a, b = map(int ,input().split())
+
+def GCD(a,b):
+    while b:
+        a,b = b, a%b
+    return a
+def GCD_DP(a,b):
+    if b == 0:
+        return a
+    return GCD_DP(b, a%b)
+
+def LCM(a,b):
+    return (a*b) // GCD_DP(a,b)
+
+print(GCD1(a,b))
+print(LCM(a,b))
