14012b4573635d450dd3e256448cd19258fbe4ac
janghana <han001112@icloud.com>
2023-01-06 09:40:46 +0900

    upload backtracking_1.py

--- Python/backtracking/backtracking_1.py
+++ Python/backtracking/backtracking_1.py
@@ -1,1 +1,22 @@
-print(ord(input()))
+N, M = map(int ,input().split())
+result = []
+visited = [False]*(N+1)
+
+def DFS():
+    if len(result) == M:
+        print(' '.join(map(str, result)))
+        return
+    for i in range(1, N+1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        result.append(i)
+        DFS()
+        result.pop()
+        # print(result)
+        # print(visited)
+        visited[i] = False
+DFS()
+
+
+
