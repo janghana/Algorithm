cded905621e01b0fc32745434f4a29c47c7d9f73
janghana <han001112@icloud.com>
2023-03-20 20:53:21 +0900

    upload the algorithm_lecture_3.py

---
+++ Python/algorithm_lecture/algorithm_lecture_3.py
@@ -0,0 +1,12 @@
+print(int(input())**2)
+print(2)
+
+'''
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        for j <- 1 to n
+            sum <- sum + A[i] × A[j]; # 코드1
+    return sum;
+}
+'''
